3fa5b74a88aebec818bfd631864d97da0b572b5f
Chris-Huang620 <mhuang5@nd.edu>
2020-10-26 11:58:00 -0700

    Made calcVarComp user facing

--- R/calcVarComp.R
+++ R/calcVarComp.R
@@ -1,24 +1,39 @@
 #' Calculate variance components.
 #'
-#' This is an internal function for CalcGTheoryCI
-#' @param x Internal input
+#' This function computes 2-facet crossed variance components using ANOVA.
+#' @param Data Data in long format in the following order: person, facet1, facet2, score
+#' @return A list of variance components
+#' @examples
+#' \dontrun{
+#' data("Brennan.3.1")
+#' calcVarComp(Data = Brennan.3.1)
+#' }
 #' @importFrom stats aggregate
 #' @export
 
-calcVarComp <- function(x) {
+calcVarComp <- function(Data = NULL) {
+
+  if(is.null(Data)) stop("The Data argument has no default.\nPlease specify an object in the specified format")
+
+  if(ncol(Data) != 4) stop("Please ensure that your data has only 4 columns, with the columns in the specified order.")
+
   ## Calculate 2-facet variance components via ANOVA from 'long' data Get sample sizes
-  np <- nlevels(x$p)
-  ni <- nlevels(x$i)
-  no <- nlevels(x$o)
+  colnames(Data) <- c("p", "i", "o", "Score")
+  Data$p <- factor(Data$p)
+  Data$i <- factor(Data$i)
+  Data$o <- factor(Data$o)
+  np <- nlevels(Data$p)
+  ni <- nlevels(Data$i)
+  no <- nlevels(Data$o)
   # Compute Sums of Squared Mean Scores (T), Brennan p.69
-  xbar <- mean(x$Score)
-  Tp <- no * ni * sum(aggregate(x[, 4], list(x$p), mean)[, 2]^2)
-  Ti <- np * no * sum(aggregate(x[, 4], list(x$i), mean)[, 2]^2)
-  To <- np * ni * sum(aggregate(x[, 4], list(x$o), mean)[, 2]^2)
-  Tpi <- no * sum(aggregate(x[, 4], list(x$p, x$i), mean)[, 3]^2)
-  Tpo <- ni * sum(aggregate(x[, 4], list(x$p, x$o), mean)[, 3]^2)
-  Tio <- np * sum(aggregate(x[, 4], list(x$o, x$i), mean)[, 3]^2)
-  Tpio <- sum(x$Score^2)
+  xbar <- mean(Data$Score)
+  Tp <- no * ni * sum(aggregate(Data[, 4], list(Data$p), mean)[, 2]^2)
+  Ti <- np * no * sum(aggregate(Data[, 4], list(Data$i), mean)[, 2]^2)
+  To <- np * ni * sum(aggregate(Data[, 4], list(Data$o), mean)[, 2]^2)
+  Tpi <- no * sum(aggregate(Data[, 4], list(Data$p, Data$i), mean)[, 3]^2)
+  Tpo <- ni * sum(aggregate(Data[, 4], list(Data$p, Data$o), mean)[, 3]^2)
+  Tio <- np * sum(aggregate(Data[, 4], list(Data$o, Data$i), mean)[, 3]^2)
+  Tpio <- sum(Data$Score^2)
   Tmu <- np * ni * no * xbar^2
   # Compute Sum of Squares (SS)
   SSp <- Tp - Tmu
@@ -44,5 +59,8 @@ calcVarComp <- function(x) {
   var_pi <- (MSpi - MSpio)/no
   var_po <- (MSpo - MSpio)/ni
   var_io <- (MSio - MSpio)/np
-  return(c(var_p, var_i, var_o, var_pi, var_po, var_io, var_pio))
+  # Return a list of seven variance components
+  var_comp <- c(var_p, var_i, var_o, var_pi, var_po, var_io, var_pio)
+  names(var_comp) <- c("var_p", "var_i", "var_o", "var_pi", "var_po", "var_io", "var_pio")
+  return(var_comp)
 }
